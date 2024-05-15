5457229f324b82b1d156b4c50be39eb48fdc9cef
Oskar Nilsson <143490244+oskarnilsson90@users.noreply.github.com>
2024-05-15 14:45:15 +0200

    Added Search exercises

--- src/Components/Exercises/Exercises.js
+++ src/Components/Exercises/Exercises.js
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 
 import { getExercises } from "@/utils/api";
 import Link from "next/link";
@@ -8,30 +8,53 @@ import styles from "@/components/Exercises/Exercises.module.css";
 
 export default function ExercisesList() {
     const [exercises, setExercises] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
     const fetchExercises = async () => {
-        const exercises = await getExercises();
-        setExercises(exercises);
+        const exercisesData = await getExercises();
+        setExercises(exercisesData);
     }
 
     useEffect(() => {
         fetchExercises();
     }, []);
 
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const filteredExercises = exercises.filter((exercise) => {
+        const lowerCaseSearchTerm = searchTerm.toLowerCase();
+        return (
+            exercise.title.toLowerCase().includes(lowerCaseSearchTerm) ||
+            exercise.category.toLowerCase().includes(lowerCaseSearchTerm)
+        );
+    });
+
     const ImageUrl = '/pitch.png';
 
-    return (<>
+    return (
+        <>
+            <div>
+                <input
+                    className={styles.searchBar}
+                    type="text"
+                    placeholder="Search exercises..."
+                    value={searchTerm}
+                    onChange={handleSearch}
+                />
+            </div>
             <div className={styles.exercisesGrid}>
-                {exercises.map(exercise => (
-                    <Link href={`/exercises/${exercise.id}`} key={exercise.id} className={styles.exerciseCard}>
+                {filteredExercises.map(exercise => (
+                    <div key={exercise.id} className={styles.exerciseCard}>
                         <Image src={ImageUrl} alt="Exercise Logo" width={100} height={100} className={styles.exerciseImage} />
                         <h2>{exercise.title}</h2>
                         <p>{exercise.category}</p>
                         <div className={styles.buttonsContainer}>
                             <Link href={`trainings`} className={styles.btn}>Add</Link>
-                            <Link href={`/exercises/${exercise.id}`} className={styles.btn}>Edit</Link>
+                            <Link href={`/exercises/${exercise.id}`} className={styles.btn}>Info</Link>
                         </div>
-                    </Link>
+                    </div>
                 ))}
             </div>
         </>
